a7e97f22d0302cb934c8c6bda19e10df7a4d6c5c
33honest <411607562@qq.com>
2018-06-01 18:36:23 +0800

    编辑上传

---
+++ src/main/java/cn/ccrm/admin/config/UeditorConfiguration.java
@@ -0,0 +1,10 @@
+package cn.ccrm.admin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class UeditorConfiguration {
+
+}
--- src/main/java/cn/ccrm/admin/web/UeditorController.java
+++ src/main/java/cn/ccrm/admin/web/UeditorController.java
@@ -1,28 +1,68 @@
 package cn.ccrm.admin.web;
 
-import java.io.UnsupportedEncodingException;
+import java.io.File;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONException;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import cn.ccrm.admin.common.ueditor.ActionEnter;
 
-@RestController
-@CrossOrigin
+
+@Controller
 @RequestMapping("/ueditor")
 public class UeditorController {
 
-	@RequestMapping(value = "/exec")
-	@ResponseBody
-	public String exec(HttpServletRequest request) throws UnsupportedEncodingException, JSONException{ 
-		request.setCharacterEncoding("utf-8");
-		String rootPath = request.getRealPath("/");
-		return new ActionEnter( request, rootPath ).exec();
-	}
-	
+    private Logger logger = LoggerFactory.getLogger(UeditorController.class);
+
+    private String rootPath;
+
+    private String projectPath = null;
+
+    @Autowired
+    private Environment environment;
+
+    private final static String staticPath = "static/";
+
+    public UeditorController() {
+        String path  = UeditorController.class.getClassLoader().getResource("config.json").getPath();
+        logger.info("path->"+path);
+        File file =  new File(path);
+        if(file.getParentFile().isDirectory()) {
+            rootPath = new File(path).getParent()+"/";
+        }else{
+            rootPath = new File(path).getParentFile().getParent()+"/";
+            rootPath = rootPath.replace("file:","");
+        }
+    }
+
+
+    private String getProjectPath(){
+        if(null==projectPath) {
+            String val = environment.getProperty("server.context-path", "");
+            if ("".equals(val)) {
+                val = environment.getProperty("server.contextPath", "");
+                if ("".equals(val)) {
+                    projectPath = "";
+                    return projectPath;
+                }
+            }
+            projectPath = val.replace("/", "") + "/";
+        }
+        return projectPath;
+    }
+
+    @RequestMapping("/exec")
+    public void exec(HttpServletRequest request, HttpServletResponse response, PrintWriter out){
+        response.setHeader("Content-Type" , "text/html");
+        logger.info("rootPath->"+rootPath+",staticPath->"+staticPath+",projectPath->"+getProjectPath());
+        out.write( new ActionEnter( request, rootPath,staticPath,getProjectPath()).exec());
+}
 }
